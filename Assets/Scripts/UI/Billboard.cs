f8971e1b8d66240a69a5c24d17b74c3b31d0513b
IAmAllOfYou <k.chiraz93@gmail.com>
2016-04-12 17:18:55 -0400

    health bar fix

--- Assets/Scripts/UI/Billboard.cs
+++ Assets/Scripts/UI/Billboard.cs
@@ -7,17 +7,27 @@ public class Billboard : MonoBehaviour
     [Tooltip("Hight of the health bar.")]
     private float hightOffset;
     private GameObject player;
+    private Transform probeModel;
     void Start()
     {
         Debug.Log("start");
-        player = GameObject.FindWithTag("Player");
+        player = transform.parent.gameObject;
+        probeModel = player.transform.FindChild("ProbeModel");
         transform.parent = null;
     }
     void Update()
     {
         transform.LookAt(Camera.main.transform);
-        Vector3 newPosition = player.transform.position;
-        newPosition.y += hightOffset;
-        transform.position = newPosition;
+        if(probeModel.name == "ProbeModel" && probeModel.gameObject.activeSelf == true)
+        {
+            Vector3 newPosition = player.transform.position;
+            newPosition.y += hightOffset;
+            transform.position = newPosition;
+        }
+        else
+        {
+            transform.gameObject.SetActive(false);
+        }
+        
     }
 }
