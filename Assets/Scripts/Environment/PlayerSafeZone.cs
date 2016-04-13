7c1ff109c624e655d0369a0e6e9b5a58a0c996fe
stellarxo <stellajhlee@hotmail.com>
2016-04-13 19:33:46 -0400

    safe zone description

--- Assets/Scripts/Environment/PlayerSafeZone.cs
+++ Assets/Scripts/Environment/PlayerSafeZone.cs
@@ -2,24 +2,26 @@
 using System.Collections;
 
 /// <summary>
-/// The player safe zone class ???
+/// The player safe zone class marks the player as 'safe'
+/// when inside a safe zone trigger collider.
 ///
 /// @author - Karl C.
 /// @version - 1.0.0
 ///
 /// </summary>
+[RequireComponent(typeof(Collider))]
 public class PlayerSafeZone : MonoBehaviour
 {
+    
+    [SerializeField]
+    private int maxSpeed = 5;
     [SerializeField]
     [Tooltip("If given a current, it will activate a current and block that path.")]
     private GameObject blockingCurrent;
 
-    /// <summary>
-    /// ???
-    /// </summary>
     void OnTriggerEnter(Collider col)
     {
-        if (col.tag == "Player") // have to put this because LightAbsorber has a player tag
+        if (col.tag == "Player")
         {
             Player player = col.GetComponent<Player>();
             if (player)
@@ -27,24 +29,18 @@ public class PlayerSafeZone : MonoBehaviour
                 player.isSafe = true;
                 if (blockingCurrent)
                 {
-                    player.MaxSpeed(5);
+                    player.MaxSpeed(maxSpeed);
                 }
             }
         }
     }
 
-    /// <summary>
-    /// ???
-    /// </summary>
     IEnumerator WaitBeforeCurrent(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
         
     }
     
-    /// <summary>
-    /// ???
-    /// </summary>
     void OnTriggerExit(Collider col)
     {
         if (col.tag == "Player")
