2bb500ac2a7a8d1fb456d7dd1c7d695800669dca
stellarxo <stellajhlee@hotmail.com>
2016-04-12 20:25:09 -0400

    player safe zone

--- Assets/Scripts/Environment/PlayerSafeZone.cs
+++ Assets/Scripts/Environment/PlayerSafeZone.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// The player safe zone class ???
+///
+/// @author - Karl C.
+/// @version - 1.0.0
+///
+/// </summary>
 public class PlayerSafeZone : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("If given a current, it will activate a current and block that path.")]
-    private GameObject blookingCurrent;
+    private GameObject blockingCurrent;
 
+    /// <summary>
+    /// ???
+    /// </summary>
     void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player") // have to put this because LightAbsorber has a player tag
@@ -15,35 +25,39 @@ public class PlayerSafeZone : MonoBehaviour
             if (player)
             {
                 player.isSafe = true;
-                if (blookingCurrent)
+                if (blockingCurrent)
                 {
                     player.MaxSpeed(5);
                 }
-                //Debug.Log(player.isSafe);
             }
         }
     }
 
+    /// <summary>
+    /// ???
+    /// </summary>
     IEnumerator WaitBeforeCurrent(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
         
     }
-
+    
+    /// <summary>
+    /// ???
+    /// </summary>
     void OnTriggerExit(Collider col)
     {
-        if (col.tag == "Player") // have to put this because LightAbsorber has a player tag
+        if (col.tag == "Player")
         {
             Player player = col.GetComponent<Player>();
             if (player)
             {
                 player.isSafe = false;
-                if (blookingCurrent)
+                if (blockingCurrent)
                 {
-                    blookingCurrent.SetActive(true);
+                    blockingCurrent.SetActive(true);
                     this.gameObject.SetActive(false);
                 }
-                //Debug.Log(player.isSafe);
             }
         }
     }
