14b614ad1c27302a727bf77a381456de2b2010a6
Alex <alex@alisterdev.com>
2016-04-15 22:02:19 -0400

    update give up

--- Assets/Scripts/NPC/Steering Behaviours/MoveClosestWaypoint.cs
+++ Assets/Scripts/NPC/Steering Behaviours/MoveClosestWaypoint.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+
 
 /// <summary>
 /// ????
@@ -12,38 +12,42 @@ using System.Collections;
 public class MoveClosestWaypoint : NPCActionable
 {
     [Tooltip("The list of waypoints that the big fish can go when the player is in a safe zone")]
-    public GameObject waypointList;
+    [SerializeField]
+    private GameObject waypointList;
+
     [Tooltip("Distance at which then big fish slows down before getting to waypoint")]
-    public float slowingRadius;
-    private Transform bigFish;
+    [SerializeField]
+    private float slowingRadius;
 
     [SerializeField]
     private WallAvoidance wallAvoidance;
-    
+
+    private Transform bigFish;
+
     public MoveClosestWaypoint(int priority, string id, Transform boss) : base(priority, id)
     {
         this.SetPriority(priority);
         this.SetID(id);
         this.bigFish = boss;
     }
-    
+
     public void SetPriority(int priority)
     {
         this.priority = priority;
-        wallAvoidance.priority = priority;
+        wallAvoidance.Priority = priority;
     }
-    
+
     public void SetID(string id)
     {
         this.id = id;
-        wallAvoidance.id = id;
+        wallAvoidance.Id = id;
     }
-    
+
     public void SetBigFish(Transform boss)
     {
         this.bigFish = boss;
     }
-    
+
     public void SetWallAvoidanceProperties(float strengthMultiplier, float avoidanceForce, float maxViewDistance, LayerMask obstacleLayer)
     {
         wallAvoidance.strengthMultiplier = strengthMultiplier;
@@ -51,7 +55,7 @@ public class MoveClosestWaypoint : NPCActionable
         wallAvoidance.MaxViewDistance = maxViewDistance;
         wallAvoidance.ObstacleLayer = obstacleLayer;
     }
-    
+
     public override void Execute(Steerable steerable)
     {
         // Override the steerable's min/max speed
@@ -65,9 +69,9 @@ public class MoveClosestWaypoint : NPCActionable
         {
             steerable.MaxForce = maxForce;
         }
-        
+
         //checks if the boss fish is at the waypoint, if so then no need to move to waypoint anymore
-        if((Vector2)bigFish.GetComponent<Rigidbody>().velocity == Vector2.zero)
+        if ((Vector2)bigFish.GetComponent<Rigidbody>().velocity == Vector2.zero)
         {
             ActionCompleted();
         }
@@ -75,7 +79,7 @@ public class MoveClosestWaypoint : NPCActionable
         {
             steerable.AddMoveWaypointForce(waypointList, bigFish, slowingRadius, strengthMultiplier);
         }
-        
+
         wallAvoidance.Execute(steerable);
     }
-} 
+}
